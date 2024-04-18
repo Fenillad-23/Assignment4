24598c62bfd82e6b253435687bee70926d16a23f
fenil lad <fenillad087@gmail.com>
2024-04-18 06:32:39 -0400

    implemented new api for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,19 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+router.get('/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 router.post('/add', async (req, res) => {
     const user = new User({
